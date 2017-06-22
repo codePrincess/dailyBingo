abe42a9f89e02031d86172e6e87cfc20032e74e9
Manu Rink <manuela.schrenk@gmail.com>
2017-06-22 12:36:32 +0200

    implemented push notifications and added app icon 😄

--- DailyBingo/AppDelegate.swift
+++ DailyBingo/AppDelegate.swift
@@ -1,46 +1,82 @@
 //
 //  AppDelegate.swift
-//  DailyBingo
+//  dailyquote
 //
-//  Created by Manu Rink on 22.06.17.
-//  Copyright © 2017 microsoft. All rights reserved.
+//  Created by Manu Rink on 23/10/16.
+//  Copyright © 2016 byteroyal. All rights reserved.
 //
 
 import UIKit
+import Foundation
+import MobileCenter
+import MobileCenterAnalytics
+import MobileCenterCrashes
+import MobileCenterPush
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, MSPushDelegate {
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        MSPush.setDelegate(self)
+        MSMobileCenter.start("c0f57360-e4d6-4efd-8e61-b6351a25b8cc",
+                             withServices: [MSAnalytics.self, MSCrashes.self, MSPush.self])
+        
         return true
     }
-
+    
+    func push(_ push: MSPush!, didReceive pushNotification: MSPushNotification!) {
+        var message: String = pushNotification.message
+        for item in pushNotification.customData {
+            message = String(format: "%@\n%@: %@", message, item.key, item.value)
+        }
+        let alert = UIAlertView(title: pushNotification.title, message: message, delegate: self, cancelButtonTitle: "OK")
+        alert.show()
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        /*let lastRefreshed = UserDefaults.standard.object(forKey: "lastRefreshed") as? Date
+         if let refreshed = lastRefreshed {
+         let calendar = NSCalendar.current
+         
+         let res = calendar.compare(refreshed, to: Date(), toGranularity: .day)
+         
+         if res == .orderedSame {
+         ViewController.refreshQuotes()
+         UserDefaults.standard.set(Date(), forKey: "lastRefreshed")
+         }
+         }
+         else {
+         ViewController.refreshQuotes()
+         UserDefaults.standard.set(Date(), forKey: "lastRefreshed")
+         }*/
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
--- DailyBingo/ViewController.swift
+++ DailyBingo/ViewController.swift
@@ -1,25 +1,136 @@
 //
 //  ViewController.swift
-//  DailyBingo
+//  dailyquote
 //
-//  Created by Manu Rink on 22.06.17.
-//  Copyright © 2017 microsoft. All rights reserved.
+//  Created by Manu Rink on 23/10/16.
+//  Copyright © 2016 byteroyal. All rights reserved.
 //
 
 import UIKit
+import Foundation
+import MobileCenterAnalytics
+
+
+enum MyError : Error {
+    case HockeyAppTestError
+}
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    static let baseURL = "https://myrandomfuncs.azurewebsites.net/api/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGR = UITapGestureRecognizer.init(target: self, action: #selector(ViewController.fetchQuote))
+        view.addGestureRecognizer(tapGR)
+        
+        let swipeGR = UISwipeGestureRecognizer.init(target: self, action: #selector(ViewController.crash))
+        swipeGR.direction = .right
+        view.addGestureRecognizer(swipeGR)
+        
+        self.quoteLabel.text = " ... "
+        
+        fetchQuote()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    static func refreshQuotes () {
+        let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+        var task : URLSessionDataTask
+        let endpoint = "SeedBingo"
+        
+        let url = NSURL(string: baseURL + endpoint)
+        
+        task = defaultSession.dataTask(with: url! as URL) {
+            data, response, error in
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    print("SUCCESS: seed of quotes")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchQuote () {
+        let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+        var task : URLSessionDataTask
+        let endpoint = "BullshitBingo?maxlength=180"
+        
+        let url = NSURL(string: ViewController.baseURL + endpoint)
+        
+        task = defaultSession.dataTask(with: url! as URL) {
+            data, response, error in
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    
+                    DispatchQueue.main.async {
+                        do {
+                            self.fadeoutLabel()
+                            
+                            let jsonData = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! String
+                            let decData = jsonData.data(using: .utf8)
+                            let dict = try JSONSerialization.jsonObject(with: decData!) as? Dictionary<String, Any>
+                            
+                            if let dictionary = dict {
+                                
+                                if let theStrings = dictionary["strings"] as? [String] {
+                                    let quote = theStrings.first
+                                    self.quoteLabel.text = "\"\(quote!)\""
+                                }
+                                
+                                self.fadeinLabel()
+                            }
+                            
+                        } catch let error as NSError {
+                            print(error)
+                        }
+                        
+                    }
+                    print("SUCCESS: fetched quote")
+                    
+                    MSAnalytics.trackEvent("quoted-fetched-by-user-on-tap")
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    private func fadeoutLabel () {
+        UIView.animate(withDuration: 1.0) {
+            self.quoteLabel.alpha = 0
+        }
+        
+    }
+    
+    private func fadeinLabel () {
+        UIView.animate(withDuration: 0.3) {
+            self.quoteLabel.alpha = 1
+        }
+    }
+    
+    func crash () throws {
+        MSAnalytics.trackEvent("crash-triggered-on-swipe-by-user")
+        
+        throw MyError.HockeyAppTestError
+    }
+    
 }
 
